3ba55a9d897b43d9eb21559835581f4c7ddcd8ab
Mad Argonian <zhuko.13@mail.ru>
2021-02-14 15:28:45 +0200

    5. Отрисуй меня полностью (часть 1)

--- js/utils.js
+++ js/utils.js
@@ -9,7 +9,7 @@
 
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 const getRandomIntInclusive = function (min, max) {
-  if (min >= max) {
+  if (min <= max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
